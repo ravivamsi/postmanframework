df9e1698fd98fa4cf5a69e3120cbe66d9e3ac348
Vamsi Ravi <ravivamsi@yahoo.com>
2018-05-24 15:22:50 -0500

    Included the application.js and pack.json for another collection execution

---
+++ application.js
@@ -0,0 +1,20 @@
+var newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('./PostmanCollection/Sample.postman_collection.json'),
+    environment: require('./PostmanEnvironment/Test.postman_environment.json'),
+    reporters: ['html','cli'],
+    reporter : { html : { export : './report/TestReport.html'}},
+    insecure: true, // allow self-signed certs, required in postman too
+    timeout: 180000  // set time out
+}).on('start', function (err, args) { // on start of run, log to console
+    console.log('running a collection...');
+}).on('done', function (err, summary) {
+    if (err || summary.error) {
+        console.error('collection run encountered an error.');
+    }
+    else {
+        console.log('collection run completed.');
+    }
+});
